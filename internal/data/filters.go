84c6e9fc5ff9ef2a2e2b5432494a6cb434203bde
cnugroho11 <cnugroho211@gmail.com>
2023-12-19 18:07:46 +0700

    progress

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,33 @@
 package data
 
-import "greenlight.cnugroho.com/internal/validator"
+import (
+	"greenlight.cnugroho.com/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafeList []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("usafe sort parameter : " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
